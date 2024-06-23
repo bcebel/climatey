9d69749836904c93b6c7a778fecec5b81b310a35
bcebel <bcebel@gmail.com>
2024-06-23 01:05:27 -0500

    trying out product images and content

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -5,11 +5,9 @@ db.once("open", async () => {
   await Category.deleteMany();
 
   const categories = await Category.insertMany([
-    { name: "Lavender" },
-    { name: "Cosmic Cat" },
-    { name: "Unscented" },
-    { name: "10 LB" },
-    { name: "20 LB" },
+    { name: "Kitty Litter" },
+    { name: "Carbon Credits" },
+    { name: "Garden" },
     //     { name: [Artist] },
   ]);
 
@@ -19,42 +17,88 @@ db.once("open", async () => {
 
   const products = await Product.insertMany([
     {
-      name: "Lavender Fields Forever",
-      description:
-        "Like Woodstock, but with more lavender, or I dunno like Pike's Place Market in Seattle, but with more lavender.",
-      artist: "Super Lightweight, super absorbent, super lavender-y",
-      image: "/images/cat1.jpeg",
+      name: "Lavender Litter",
+      description: "Lavender Fields Furrever.",
+      artist: "Super Lightweight, absorbent and clumping kitty litter",
+      image: "/images/cat3.jpeg",
       category: categories[0]._id,
       price: 20.99,
       quantity: 1,
     },
     {
-      name: "Cosmic Cat Litter",
+      name: "Cosmic Kitty Litter",
       description: "Totally groovy cat litter for your totally groovy cat.",
-      artist: "Montgomery Montague",
-      image: "/images/cat2.jpeg",
-      category: categories[1]._id,
-      price: 109.99,
+      artist: "Frankensense Aromatherapy for your cat.",
+      image: "/images/cat4.jpeg",
+      category: categories[0]._id,
+      price: 20.99,
       quantity: 2,
     },
     {
-      name: "Happy Days",
-      description: "Neon Car 50s",
-      artist: "Fonzie Heyy",
-      image: "/images/cat3.jpeg",
-      category: categories[2]._id,
-      price: 200.99,
+      name: "Unscented Cat Litter",
+      description:
+        "Unscented Cat Litter for the discerning cat owner.  10 LB bag.",
+      artist: "Simple, clean, and effective.",
+      image: "/images/cat8.png",
+      category: categories[0]._id,
+      price: 20.99,
       quantity: 1,
     },
     {
-      name: "Moo",
-      description: "Neon Cow",
-      artist: "Fawn Z. Hay",
-      image: "/images/cat4.jpeg",
-      category: categories[3]._id,
-      price: 200.99,
+      name: "Carbon Credits",
+      description: "1 ton of CO2 offset",
+      artist: "Thank you for offsetting carbon emissions!",
+      image: "/images/carboncredit.jpeg",
+      category: categories[1]._id,
+      price: 200,
       quantity: 1,
-    }
+    },
+    {
+      name: "Carbon Footprints Stepping Stones",
+      description: "1 Foot by 1 Foot square stepping stones.",
+      artist: "Large Square Tiles",
+      image: "/images/pavers.jpeg",
+      category: categories[2]._id,
+      price: 20,
+      quantity: 1,
+    },
+    {
+      name: "Carbon Footprints Stepping Stones",
+      description: "6 inch by 6 inch square stepping stones.",
+      artist: "Small Square Tiles",
+      image: "/images/pavers.jpeg",
+      category: categories[2]._id,
+      price: 20,
+      quantity: 1,
+    },
+    {
+      name: "Carbon Footprints Stepping Stones",
+      description: "6 inch by 12 inch rectangular stepping stones.",
+      artist: "Rectangle Tiles",
+      image: "/images/pavers.jpeg",
+      category: categories[2]._id,
+      price: 20,
+      quantity: 1,
+    },
+    {
+      name: "Gnome",
+      description:
+        "Imagine Glimmerwick the Gnome standing about 12 inches tall, with a cheerful expression on his face.  Glimmerwick's attire is crafted from textured, carbonized cement, which gives him a weathered appearance that blends harmoniously with garden surroundings.",
+      artist: "Fawn Z. Hay",
+      image: "/images/gnome.jpeg",
+      category: categories[2]._id,
+      price: 200,
+      quantity: 1,
+    },
+    {
+      name: "Flower Pot",
+      description: "An elegant carbonized cement flower pot.",
+      artist: "C. Ment",
+      image: "/images/flowerpot.jpeg",
+      category: categories[2]._id,
+      price: 200,
+      quantity: 1,
+    },
   ]);
 
   console.log("products seeded");
